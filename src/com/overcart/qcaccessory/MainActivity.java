2f91c5506985d0bb7d89117e3821b3918bd4e797
shikhar91939 <shikhar.91939@gmail.com>
2015-03-18 16:15:07 +0530

    added 2nd screen

--- src/com/overcart/qcaccessory/MainActivity.java
+++ src/com/overcart/qcaccessory/MainActivity.java
@@ -1,9 +1,11 @@
 package com.overcart.qcaccessory;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -32,5 +34,10 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    public void onClick_next(View view){
+    	// save the fields entered
+    	Intent intent = new Intent(getApplicationContext(),Page1_QRscan.class);
+    	startActivity(intent);
     }
 }
---
+++ src/com/overcart/qcaccessory/Page1_QRscan.java
@@ -0,0 +1,17 @@
+package com.overcart.qcaccessory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Page1_QRscan extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    setContentView(R.layout.page1_qrscan);
+	}
+
+	
+}
