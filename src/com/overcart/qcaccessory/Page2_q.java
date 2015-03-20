001fec35c77421fc8e0dd85b5368af11c357d43a
shikhar91939 <shikhar.91939@gmail.com>
2015-03-20 19:20:29 +0530

    form completion added and named all edi texts. Then run once

--- src/com/overcart/qcaccessory/Page2_q.java
+++ src/com/overcart/qcaccessory/Page2_q.java
@@ -3,8 +3,12 @@ package com.overcart.qcaccessory;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Page2_q extends Activity {
+	
+	EditText et_brandName, et_modelId;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -12,8 +16,15 @@ public class Page2_q extends Activity {
 	    super.onCreate(savedInstanceState);
 	
 	    setContentView(R.layout.page2_q);
+	    et_brandName = (EditText) findViewById(R.id.et_brandName);
+	    et_modelId = (EditText) findViewById(R.id.et_modelId);
 	}
 	public void onClick_next(View view){
 		
+		if (et_brandName.length()>0 && et_modelId.length()>0){
+			Toast.makeText(getBaseContext(),"form complete", Toast.LENGTH_SHORT).show();
+		}else{
+			Toast.makeText(getBaseContext(),"form INcomplete", Toast.LENGTH_SHORT).show();
+		}
 	}
 }
